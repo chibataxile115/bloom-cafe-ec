e18748b3eb591d8371c6c937bc336130b0bbde5f
yuhgo <yuhgo090764@gmail.com>
2022-02-13 01:08:13 +0900

    🗒 Redux Toolkitの初期設定

---
+++ src/pages/_app.tsx
@@ -0,0 +1,16 @@
+import '../styles/globals.css'
+import type { AppProps } from 'next/app'
+// NOTE: Redux関連
+import { Provider } from 'react-redux'
+import { store } from '../redux/app/store'
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  )
+}
+
+
+export default MyApp
