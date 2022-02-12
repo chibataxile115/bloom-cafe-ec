d46cbc42861c854bcb40325f99b4bfba0f16d848
yuhgo <yuhgo090764@gmail.com>
2022-02-13 01:04:46 +0900

    🗒 tailwindCSSの設定

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  content: ['./public/**/*.{js,ts,jsx,tsx}', './src/**/*.{js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
