754718f7bcca820f77a563eaa336b51d78421258
yuhgo <yuhgo090764@gmail.com>
2022-02-13 01:17:00 +0900

    🗒 メインのlayoutコンポーネント

---
+++ src/components/layout/HomeLayout.tsx
@@ -0,0 +1,41 @@
+import { FC, ReactNode } from 'react'
+import Head from 'next/Head'
+import { Steper } from '../atoms'
+
+interface Props {
+  children: ReactNode
+  title: 'ようこそ' | '商品一覧' | '商品詳細' | '注文確認' | 'カート'
+}
+
+const HomeLayout: FC<Props> = (props) => {
+  const { children, title } = props
+
+  return (
+    <div>
+      <Head>
+        <title>{title}</title>
+      </Head>
+
+      <header className="mt-10 min-w-full">
+        <Steper />
+      </header>
+
+      <main className="flex w-screen flex-1 flex-col items-center">
+        {children}
+      </main>
+      <footer className="flex h-12 w-full items-center justify-center border-t ">
+        {/* FIXME: 文字色を修正 */}
+        <a
+          className="flex items-center text-black"
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          bloom{' '}
+        </a>
+      </footer>
+    </div>
+  )
+}
+
+export default HomeLayout
