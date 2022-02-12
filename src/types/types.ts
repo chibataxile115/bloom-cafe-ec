cd9f126778c4a9c234e5c5bbd4b6406918b7e92c
yuhgo <yuhgo090764@gmail.com>
2022-02-13 01:17:32 +0900

    🗒 型を追加

---
+++ src/types/types.ts
@@ -0,0 +1,15 @@
+export interface User {
+  liffID: string
+  userName: string
+}
+
+export interface MenueItems {
+  id: number
+  plice: number
+  name: string
+  quantity: number
+}
+
+export interface Step {
+  stepIndex: number
+}
