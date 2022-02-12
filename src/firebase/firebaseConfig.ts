4252d9d43cc9a3b2c97176a5547add1bbcdcae60
yuhgo <yuhgo090764@gmail.com>
2022-02-13 01:17:50 +0900

    🗒 Firebaseの設定ファイル

---
+++ src/firebase/firebaseConfig.ts
@@ -0,0 +1,24 @@
+import { initializeApp } from 'firebase/app'
+import { getAuth, GoogleAuthProvider } from 'firebase/auth'
+import { getFirestore } from 'firebase/firestore'
+import { getStorage } from 'firebase/storage'
+// import { getAnalytics } from 'firebase/analytics'
+
+// 初期化
+const firebaseApp = initializeApp({
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_APIKEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+})
+
+export const DB = getFirestore(firebaseApp)
+export const Auth = getAuth(firebaseApp)
+export const GoogleProvider = new GoogleAuthProvider()
+export const Storage = getStorage(firebaseApp)
+
+// アナリティクス
+// export const analytics = getAnalytics(firebaseApp)
