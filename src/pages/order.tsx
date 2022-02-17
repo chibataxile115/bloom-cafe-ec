7f5f999b807e80a57e184de05b698733af7e049d
chiba <chibaxile0000115@gmail.com>
2022-02-17 23:48:19 +0900

    ページ土台作成

--- src/pages/order.tsx
+++ src/pages/order.tsx
@@ -1,7 +1,14 @@
 import React from 'react'
+// NOTE: original
+import { HomeLayout } from '../components/layout'
+import { OrderBase } from '../components/pages'
 
 const order = () => {
-  return <div>order</div>
+  return (
+    <div>
+      <OrderBase />
+    </div>
+  )
 }
 
 export default order
