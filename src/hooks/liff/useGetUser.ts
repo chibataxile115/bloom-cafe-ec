e98960705f6794c0beab8a0e4e20e348bff57448
chiba <chibaxile0000115@gmail.com>
2022-02-17 22:34:26 +0900

    LIFF用のカスタムフック ・liffのインスタンス化 ・メッセージの送信

---
+++ src/hooks/liff/useGetUser.ts
@@ -0,0 +1,7 @@
+import { VFC, useState, useEffect } from 'react'
+import { useLiff } from 'react-liff'
+
+import { selectUser } from '../../redux/features/userSlice'
+import { useAppDispatch } from '../../redux/app/hooks'
+
+export const useGetUser = () => {}
