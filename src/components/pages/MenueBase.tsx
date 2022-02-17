4750694addaa011f267f4ce319e4ac620fbbd65f
chiba <chibaxile0000115@gmail.com>
2022-02-17 23:48:03 +0900

    ボタン設置・ページ遷移

--- src/components/pages/MenueBase.tsx
+++ src/components/pages/MenueBase.tsx
@@ -4,19 +4,27 @@ import { useRouter } from 'next/router'
 
 // NOTE: Redux関連
 import { useAppDispatch, useAppSelector } from '../../redux/app/hooks'
-
-
+import { selectStep, changeState } from '../../redux/features/step/stepSlice'
 
 // NOTE: original
 import { HomeLayout } from '../layout'
 import { FormItem } from '../modules'
 
 const MenueBase = () => {
+  const router = useRouter()
+  const dispatch = useAppDispatch()
+  const registClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    // stepperの更新
+    dispatch(changeState({ ...selectStep, stepIndex: 3 }))
+    router.push('/order')
+  }
+
   return (
     <div>
       <HomeLayout title="商品一覧">
         menueBase
         <FormItem />
+        <button onClick={registClick}>注文確定</button>
       </HomeLayout>
     </div>
   )
--- src/components/pages/OrderBase.tsx
+++ src/components/pages/OrderBase.tsx
@@ -1,9 +1,30 @@
 import React from 'react'
+import { useRouter } from 'next/router'
 // NOTE: original
 import { HomeLayout } from '../layout'
+// NOTE: Redux関連
+import { useAppDispatch, useAppSelector } from '../../redux/app/hooks'
+import {
+  selectStep,
+  resetState,
+  changeState,
+} from '../../redux/features/step/stepSlice'
 
 const OrderBase = () => {
-  return <HomeLayout title="注文確認">OrderBase</HomeLayout>
+  const router = useRouter()
+  const dispatch = useAppDispatch()
+  const registClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    // stepperの更新
+    dispatch(changeState({ ...selectStep, stepIndex: 1 }))
+    router.push('/')
+  }
+
+  return (
+    <HomeLayout title="注文確認">
+      <button onClick={registClick}>もう一度</button>
+      <button>注文確定</button>
+    </HomeLayout>
+  )
 }
 
 export default OrderBase
