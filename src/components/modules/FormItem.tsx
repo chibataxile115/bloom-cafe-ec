bc9985b1c3eb55b53f08b1512ed2b4ee0fa8cdc0
chiba <chibaxile0000115@gmail.com>
2022-02-19 22:38:23 +0900

    テキストフォーム追加

--- src/components/modules/FormItem.tsx
+++ src/components/modules/FormItem.tsx
@@ -9,7 +9,39 @@ const FormItem = () => {
         fullWidth
         margin="normal"
         type="time"
+        helperText="配達時間を選択してください"
+      ></TextField>
+      <TextField
+        size="small"
+        fullWidth
+        margin="normal"
+        type="date"
         helperText="配達日を選択してください"
+      ></TextField>
+      <TextField
+        size="small"
+        fullWidth
+        margin="normal"
+        helperText="電話番号を入力してください"
+      ></TextField>
+      <TextField
+        size="small"
+        label="名前"
+        fullWidth
+        margin="normal"
+        helperText="お客様名、会社名を入力してください"
+      ></TextField>
+      <TextField
+        size="small"
+        fullWidth
+        margin="normal"
+        helperText="配達先住所を入力してください"
+      ></TextField>
+      <TextField
+        size="small"
+        fullWidth
+        margin="normal"
+        helperText="備考を入力してください"
       ></TextField>
     </div>
   )
