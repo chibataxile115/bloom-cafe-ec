dd4734923f4c72ccb5668ea4a38d829e6ef1f5db
chiba <chibaxile0000115@gmail.com>
2022-02-24 00:00:28 +0900

    カートボタン追加

---
+++ src/components/modules/menueparts/CartButton.tsx
@@ -0,0 +1,16 @@
+import React, { useState, useCallback } from 'react'
+import { Button } from '@material-ui/core'
+
+const CartButton = ( ) => {
+  
+
+
+  return (
+    <div>
+      <button id="add" className="" >
+        カートに追加する
+      </button>
+    </div>
+  )
+}
+export default CartButton
