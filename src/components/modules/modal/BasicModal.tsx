9a3d770a7d23ab45da2c14d0534df732458e2cf0
ゆうご <yuhgo090764@gmail.com>
2022-04-28 19:26:51 +0900

    📒 メニューの新規登録用モーダルを作成 - フォームのバリデーション - Firebaseとの通信時のローディング表示

---
+++ src/components/modules/modal/BasicModal.tsx
@@ -0,0 +1,41 @@
+interface Props {
+  children: React.ReactNode
+  isOpenModal: boolean
+}
+
+const BasicModal: React.FC<Props> = (props) => {
+  const { children, isOpenModal } = props
+
+  return (
+    <>
+      {isOpenModal && (
+        <div
+          className="
+          fixed
+          top-0 left-0 z-10
+          flex
+          h-full w-full items-center
+          justify-center
+          bg-gray-600 bg-opacity-30
+
+          "
+        >
+          <div
+            className="
+            flex
+            max-h-[90%]
+
+            min-w-[500px] flex-col overflow-y-scroll
+            rounded-md
+            bg-white p-10 shadow-2xl
+          "
+          >
+            {children}
+          </div>
+        </div>
+      )}
+    </>
+  )
+}
+
+export default BasicModal
