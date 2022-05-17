fb9a243af973323e299ee3ec68fd0d373dabe553
ゆうご <yuhgo090764@gmail.com>
2022-05-18 00:10:33 +0900

    📒 nextImage用のloader関数

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,2 +1,3 @@
 export { default as MenueRegistFormSchema } from './MenueRegistFormSchema'
 export { default as ConvertFirestoreData } from './ConvertFirestoreData'
+export { default as ImageLoader } from './ImageLoader'
