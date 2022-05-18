bffe94f2671bf734f8e39a783b1104d2867523fb
ゆうご <yuhgo090764@gmail.com>
2022-05-18 14:42:08 +0900

    📒 一部修正 ・snackbarのスタイルを修正 ・React.FCの追加 ・不要なexportを削除

--- src/components/modules/modal/index.ts
+++ src/components/modules/modal/index.ts
@@ -1,4 +1,3 @@
 export { default as BasicModal } from './BasicModal'
-export { default as MenueSubmitModal } from './MenueSubmitModal'
 export { default as CartDetailModal } from './CartDetailModal'
 export { default as ImageCartModal } from './CartDetailImage'
