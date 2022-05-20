4d8b8381e3d344d8e9612035d8a183071baf2631
ゆうご <yuhgo090764@gmail.com>
2022-05-21 00:15:32 +0900

    📒 スピナーをTailwindCSSに変更

--- src/components/atoms/index.ts
+++ src/components/atoms/index.ts
@@ -1,3 +1,4 @@
+export { default as Spinner } from './Spinner'
 export { default as Steper } from './Steper'
 export { default as SnackBar } from './SnackBar'
 export { default as Tabs } from './Tabs'
