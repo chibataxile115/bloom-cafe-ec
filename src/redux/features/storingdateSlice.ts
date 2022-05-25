0ce253d1baaf74ea7e9b7e007960180f91e88b22
chiba <chibaxile0000115@gmail.com>
2022-05-25 23:34:48 +0900

    state更新処理変更

--- src/redux/features/storingdateSlice.ts
+++ src/redux/features/storingdateSlice.ts
@@ -9,7 +9,8 @@ interface UpdateArg {
 
 interface UpdateArg2 {
   targetIndex: number
-  updateCart: boolean
+  isInCartState: boolean
+  countState: number
 }
 
 // NOTE: initialStateを定義
@@ -22,9 +23,6 @@ export const storingDataSlice = createSlice({
   reducers: {
     resetOrder: (state) => {
       state.splice(0)
-    },
-    updatedCart: (state, action: PayloadAction<UpdateArg2>) => {
-      state[action.payload.targetIndex].isInCart = action.payload.updateCart
     },
 
     // 商品を登録
@@ -58,11 +56,17 @@ export const storingDataSlice = createSlice({
         }
       },
     },
+    // 商品を更新
+    updateDetail: (state, action: PayloadAction<UpdateArg2>) => {
+      const { targetIndex, isInCartState, countState } = action.payload
+      state[targetIndex].isInCart = isInCartState
+      state[targetIndex].count = countState
+    },
   },
 })
 
 // NOTE: actionsをエクスポートする
-export const { resetOrder, addOrder, updatedCart } = storingDataSlice.actions
+export const { resetOrder, addOrder, updateDetail } = storingDataSlice.actions
 // NOTE: reducerをエクスポートする
 export const selectStoringData = (state: RootState) => state.storingData
 // NOTE: selectorをエクスポートする
