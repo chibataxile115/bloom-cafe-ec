6092e28dcd6f06cb23096bdd772220159f873987
chiba <chibaxile0000115@gmail.com>
2022-06-15 12:31:01 +0900

    index追加

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,3 +1,4 @@
 export { default as MenueRegistFormSchema } from './MenueRegistFormSchema'
+export { default as OrderFormSchema } from './OrderFormSchema'
 export { default as ConvertFirestoreData } from './ConvertFirestoreData'
 export { default as ImageLoader } from './ImageLoader'
