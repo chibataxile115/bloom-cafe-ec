342a4272b72d1963136f1ba5292372431514f976
chiba <chibaxile0000115@gmail.com>
2022-06-17 14:26:37 +0900

    名前修正

--- src/pages/client.tsx
+++ src/pages/client.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 // NOTE: original
-import { ClientBase } from '../components/pages'
+import { ClientFormBase } from '../components/pages'
 
 const form = () => {
-  return <ClientBase />
+  return <ClientFormBase />
 }
 
 export default form
