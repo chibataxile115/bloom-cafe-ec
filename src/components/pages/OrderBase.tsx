ae83c215c58845eecc85ed405d21b58bac61191b
chiba <chibaxile0000115@gmail.com>
2022-06-17 22:06:20 +0900

    ステッパー遷移変更

--- src/components/pages/OrderBase.tsx
+++ src/components/pages/OrderBase.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { useRouter } from 'next/router'
 // NOTE: original
 import { HomeLayout } from '../layout'
-import { FormItem } from '../modules/orderPageParts'
+import { OrderMenueView } from '../modules/orderPageParts'
 // NOTE: Redux関連
 import { useAppDispatch, useAppSelector } from '../../redux/app/hooks'
 import { selectStep, changeState } from '../../redux/features/step/stepSlice'
@@ -12,13 +12,13 @@ const OrderBase = () => {
   const dispatch = useAppDispatch()
   const registClick = (e: React.MouseEvent<HTMLButtonElement>) => {
     // stepperの更新
-    dispatch(changeState({ ...selectStep, stepIndex: 1 }))
+    dispatch(changeState({ ...selectStep, stepIndex: 3 }))
     router.push('/')
   }
 
   return (
     <HomeLayout title="注文確認">
-      <></>
+      <OrderMenueView />
     </HomeLayout>
   )
 }
