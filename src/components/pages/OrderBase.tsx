b348825193f6e388550fb26f4999c88822efbbe0
chiba <chibaxile0000115@gmail.com>
2022-06-18 18:20:43 +0900

    注文確認画面：注文者情報追加

--- src/components/pages/OrderBase.tsx
+++ src/components/pages/OrderBase.tsx
@@ -2,23 +2,14 @@ import React from 'react'
 import { useRouter } from 'next/router'
 // NOTE: original
 import { HomeLayout } from '../layout'
-import { OrderMenueView } from '../modules/orderPageParts'
+import { OrderMenueView, OrderClientView } from '../modules/orderPageParts'
 // NOTE: Redux関連
-import { useAppDispatch, useAppSelector } from '../../redux/app/hooks'
-import { selectStep, changeState } from '../../redux/features/step/stepSlice'
 
 const OrderBase = () => {
-  const router = useRouter()
-  const dispatch = useAppDispatch()
-  const registClick = (e: React.MouseEvent<HTMLButtonElement>) => {
-    // stepperの更新
-    dispatch(changeState({ ...selectStep, stepIndex: 3 }))
-    router.push('/')
-  }
-
   return (
     <HomeLayout title="注文確認">
       <OrderMenueView />
+      <OrderClientView />
     </HomeLayout>
   )
 }
