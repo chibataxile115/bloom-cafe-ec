599d4398c87b62b98a25128befe2891318480f45
chiba <chibaxile0000115@gmail.com>
2022-06-22 00:12:59 +0900

    index追加

--- src/components/modules/orderPageParts/index.ts
+++ src/components/modules/orderPageParts/index.ts
@@ -1,2 +1,3 @@
 export { default as OrderMenueView } from './OrderMenueView'
+export { default as OrderConfiButton } from './OrderConfiButton'
 export { default as OrderClientView } from './OrderClientView'
