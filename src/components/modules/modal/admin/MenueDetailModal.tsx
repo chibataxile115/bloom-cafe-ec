6254892ccd512c98be8365aa6807ad8b9efd70b1
ゆうご <yuhgo090764@gmail.com>
2022-06-23 19:02:26 +0900

    📒 説明の編集機能を実装

--- src/components/modules/modal/admin/MenueDetailModal.tsx
+++ src/components/modules/modal/admin/MenueDetailModal.tsx
@@ -324,33 +324,107 @@ const MenueDetailModal: React.FC = () => {
 
           {/* 説明文 */}
           <tr className="mt-5 flex flex-col">
-            <th className="flex justify-start">
-              <label htmlFor="detail-description">説明</label>
+            <th>
+              <div className="flex items-center justify-between">
+                <label
+                  htmlFor="detail-description"
+                  className="text-lg text-gray-700"
+                >
+                  説明
+                  {adminMenueDetailSelector.isDescriptionEdit && '(編集モード)'}
+                </label>
+                <button
+                  className={clsx(
+                    `
+                      items-center
+                      rounded
+                      border-2 border-gray-400
+                      p-1
+                      shadow
+                      hover:border-gray-700
+                      hover:shadow-lg
+                      `,
+                    adminMenueDetailSelector.isDescriptionEdit
+                      ? `bg-green-300 hover:bg-green-400`
+                      : `bg-blue-300 hover:bg-blue-400`
+                  )}
+                  onClick={() =>
+                    dispatch(
+                      changeStateForAdminMenuDetail({
+                        ...adminMenueDetailSelector,
+                        isDescriptionEdit:
+                          !adminMenueDetailSelector.isDescriptionEdit,
+                      })
+                    )
+                  }
+                >
+                  {adminMenueDetailSelector.isDescriptionEdit ? (
+                    <>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="h-5 w-5 text-green-700"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                        stroke-width="2"
+                      >
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          d="M8 7H5a2 2 0 00-2 2v9a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-3m-1 4l-3 3m0 0l-3-3m3 3V4"
+                        />
+                      </svg>
+                    </>
+                  ) : (
+                    <>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="h-5 w-5 text-blue-700"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                        stroke-width="2"
+                      >
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"
+                        />
+                      </svg>
+                    </>
+                  )}
+                </button>
+              </div>
             </th>
             <td className="mt-2">
               <textarea
-                className="
-                form-control
-                focus:shadow-outline
-                m-0
-                block
-                w-full
-                rounded
-                border
-                border-solid
-                border-gray-300 bg-white
-                bg-clip-padding px-3 py-1.5
-                text-base
-                font-normal
-                text-gray-700
-                transition
-                ease-in-out focus:border-blue-600 focus:bg-white
-                focus:text-gray-700
-                "
+                className={clsx(
+                  `
+                  focus:shadow-outline
+                  w-full appearance-none rounded
+                  border  py-2
+                  px-3
+                  leading-tight
+                  text-gray-700
+                  `,
+                  adminMenueDetailSelector.isDescriptionEdit
+                    ? `bg-white shadow`
+                    : `border-transparent bg-gray-200 shadow-transparent`
+                )}
                 id="detail-description"
                 cols={35}
                 rows={5}
                 placeholder="商品の説明"
+                disabled={!adminMenueDetailSelector.isDescriptionEdit}
+                value={adminMenueDetailSelector.description}
+                onChange={(event: React.ChangeEvent<HTMLTextAreaElement>) => {
+                  dispatch(
+                    changeStateForAdminMenuDetail({
+                      ...adminMenueDetailSelector,
+                      description: event.target.value,
+                    })
+                  )
+                }}
               ></textarea>
             </td>
           </tr>
