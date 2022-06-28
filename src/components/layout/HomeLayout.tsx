b04fabc012590b52e368f378ca527cfd3364e138
ゆうご <yuhgo090764@gmail.com>
2022-06-29 00:00:40 +0900

    📒 カートアイコンをstickyに変更

--- src/components/layout/HomeLayout.tsx
+++ src/components/layout/HomeLayout.tsx
@@ -13,27 +13,11 @@ import {
 
 interface Props {
   children: ReactNode
-  title:
-    | 'ようこそ'
-    | '商品一覧'
-    | '商品詳細'
-    | '注文確認'
-    | '情報入力'
-    | 'カート'
+  title: 'ようこそ' | '商品選択' | 'お客様情報' | '注文内容'
 }
 
 const HomeLayout: FC<Props> = (props) => {
   const { children, title } = props
-
-  const dispatch = useAppDispatch()
-  const cartDetailSelector = useAppSelector(selectCartDetail)
-  const menuPageSelector = useAppSelector(selectMenuePage)
-
-  const openModal = () => {
-    dispatch(
-      changeStateForMenuePage({ ...menuPageSelector, isOpenCartModal: true })
-    )
-  }
 
   return (
     <div
@@ -51,19 +35,6 @@ const HomeLayout: FC<Props> = (props) => {
         <Stepper />
       </header>
       <main className="flex w-screen flex-1 flex-col items-center">
-        <div className="ml-auto mr-auto flex h-6 w-6 justify-center rounded-full bg-gray-400 text-white ">
-          <p>{cartDetailSelector.totalCount}</p>
-        </div>
-        <span className="float-right " onClick={openModal}>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-5 w-5"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-          >
-            <path d="M3 1a1 1 0 000 2h1.22l.305 1.222a.997.997 0 00.01.042l1.358 5.43-.893.892C3.74 11.846 4.632 14 6.414 14H15a1 1 0 000-2H6.414l1-1H14a1 1 0 00.894-.553l3-6A1 1 0 0017 3H6.28l-.31-1.243A1 1 0 005 1H3zM16 16.5a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0zM6.5 18a1.5 1.5 0 100-3 1.5 1.5 0 000 3z" />
-          </svg>
-        </span>
         {children}
       </main>
       <footer className="sticky bottom-0 flex h-10 w-full items-center justify-center border-t border-white bg-gray-200">
